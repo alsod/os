52d99c4e585f8dece75a44da28bd708a43fe5e23
alsod <balsod@gmail.com>
2010-05-11 23:04:05 +0200

    slut på dag

--- 3/malloc.c
+++ 3/malloc.c
@@ -2,6 +2,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
+#include <string.h>
 
 #define FIRST_FIT 1
 #define BEST_FIT  2
@@ -13,7 +14,7 @@ int STRATEGY = 1;
 #endif
 
 #ifndef DNRQUICKLISTS
-int DNRQUICKLISTS = 5;
+#define DNRQUICKLISTS 2
 #endif
 
 
@@ -35,15 +36,44 @@ typedef union header Header;
 
 static Header base; /* empty list to get started */
 static Header *freep = NULL; /* start of free list */
+
+static Header quicklist_base[DNRQUICKLISTS];
+static Header quicklist_freep[DNRQUICKLISTS];
+static int list;
+
 static Header *morecore(unsigned);
+
+static Header getfreep(unsigned nunits) {
+    int pow = 8;
+    int i;
+    for (i = 0; i < DNRQUICKLISTS; i++) {
+
+        if (nunits <= pow) {
+            return quicklist_freep[i];
+        }
+        pow = pow * 2;
+    }
+    return quicklist_freep[DNRQUICKLISTS];
+}
+
+static Header getbase(unsigned nunits) {
+    int pow = 8;
+    int i;
+    for (i = 0; i < DNRQUICKLISTS; i++) {
+
+        if (nunits <= pow) {
+            return quicklist_base[i];
+        }
+        pow = pow * 2;
+    }
+    return quicklist_base[DNRQUICKLISTS];
+}
 
 void *malloc(unsigned nbytes) {
 
     // <editor-fold defaultstate="collapsed" desc="FIRST_FIT">
     if (STRATEGY == FIRST_FIT) {
-        Header *p, *prevp, *test_p, *test_prevp;
-        test_p = NULL;
-        test_prevp = NULL;
+        Header *p, *prevp;
 
         unsigned nunits;
 
@@ -194,7 +224,45 @@ void *malloc(unsigned nbytes) {
 
     // <editor-fold defaultstate="collapsed" desc="QUICK_FIT">
     if (STRATEGY == QUICK_FIT) {
-        
+        Header *p, *prevp;
+
+        unsigned nunits;
+
+        if (nbytes <= 0)
+            return NULL;
+
+        nunits = (nbytes + sizeof (Header) - 1) / sizeof (Header) + 1;
+        freep = *getfreep(nunits);
+        base = getbase(nunits);
+
+        if ((prevp = freep) == NULL) {
+            base.s.ptr = freep = prevp = &base;
+            base.s.size = 0;
+        }
+
+        /* Try to find free block */
+        for (p = prevp->s.ptr;; prevp = p, p = p->s.ptr) {
+
+            /*FIRST_FIT*/
+            if (p->s.size >= nunits) { /* big enough */
+                break;
+            }
+            /*END FIRST_FIT*/
+
+            if (p == freep) /* wrapped around free list */
+                if ((p = morecore(nunits)) == NULL)
+                    return NULL; /* none left */
+        }
+
+        if (p->s.size == nunits) { /* exactly */
+            prevp->s.ptr = p->s.ptr;
+        } else {
+            p->s.size -= nunits;
+            p += p->s.size;
+            p->s.size = nunits;
+        }
+        freep = prevp;
+        return (void *) (p + 1);
     }// </editor-fold>
 
 }
